39aa4976f8a56b863c547fc855c1bad23f15fd05
AlexisRoe <A.Roehrling@gmx.de>
2020-10-21 11:57:11 +0200

    Add styling to cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./app.css";
-/* import Header from "./components/Header"; */
 import { createElement } from "./utils/elements";
 import { getCharacterById } from "./utils/api";
 import { Character } from "./components/Character";
 
 function App() {
-  /* const header = Header() */
   const main = createElement("main");
 
   async function getCharacter() {
